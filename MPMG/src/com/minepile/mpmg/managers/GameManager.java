0d2898196e34883f53930dd5000e673b90cbda82
unenergizer <unenergizer@gmail.com>
2014-05-27 15:20:58 -0500

    Stable version commit. Version updated.

--- MPMG/src/com/minepile/mpmg/managers/GameManager.java
+++ MPMG/src/com/minepile/mpmg/managers/GameManager.java
@@ -16,7 +16,7 @@ public class GameManager {
 	private static MPMG plugin;
 	
 	private static boolean gameRunning = false;		//Mini-game is running? (default = false)
-	private static String pluginVersion = "0.3.7";	//Plugin version.
+	private static String pluginVersion = "0.3.8";	//Plugin version.
 	private static int minPlayers = 2;				//Minimal players needed to start a game.
 	private static int maxPlayers = 16;				//Maximum players allowed in a game.
 	private static MiniGameType lastGame = MiniGameType.TEAMDEATHMATCH;
